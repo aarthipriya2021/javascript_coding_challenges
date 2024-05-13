96938f6a65c6f15b714d510455debc4ace400e1f
Aarthipriya A <a.aarthipriya2021@gmail.com>
2024-05-13 11:51:13 +0530

    descriptionn , link updated

--- 27_arithmetic_operators.js
+++ 27_arithmetic_operators.js
@@ -1,6 +1,6 @@
 // May-11, 2024
 // Write a function that takes two numbers (a and b) as arguments. If a is smaller than b, divide a by b. Otherwise, multiply both numbers. Return the resulting value
-// https://www.jschallenger.com/javascript-practice/javascript-fundamentals/multiplication-division-comparison-operators
+// https://www.jschallenger.com/javascript-practice/javascript-fundamentals/multiplication-division-comparison-operators/
 // Status: SUCCESS
 
 
--- 28_decimal_number.js
+++ 28_decimal_number.js
@@ -1,6 +1,6 @@
 // May-11, 2024
-// Write a function that takes two numbers (a and b) as arguments. If a is smaller than b, divide a by b. Otherwise, multiply both numbers. Return the resulting value
-// https://www.jschallenger.com/javascript-practice/javascript-fundamentals/multiplication-division-comparison-operators
+// Write a function that takes a number (a) as argument. Round a to the 2nd digit after the decimal point. Return the rounded number
+// https://www.jschallenger.com/javascript-practice/javascript-fundamentals/round-2-decimal-digits/
 // Status: SUCCESS
 
 
